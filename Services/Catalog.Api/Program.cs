fae9b6f36bf14c02a08565d6b5852f885d0b3eff
MHGalaxy <mo.ho.kahkeshan@gmail.com>
2026-02-17 17:15:55 +0330

    Config swagger and iis

--- Services/Catalog.Api/Program.cs
+++ Services/Catalog.Api/Program.cs
@@ -5,6 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.OpenApiInfo { Title = "Catalog.Api", Version = "v1", Description = "Catalog API" });
+}); //Create document of swagger
 
 var app = builder.Build();
 
@@ -12,6 +16,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "Store API v1");
+        options.RoutePrefix = "swagger"; // /swagger
+    });
 }
 
 app.UseAuthorization();
