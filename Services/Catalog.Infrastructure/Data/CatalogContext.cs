35f72388ace139545c53fdc37165973a4bb3bd1b
Mohammad Hosein Kahkeshan <mo.ho.kahkeshan@gmail.com>
2026-02-26 15:06:23 +0330

    Create CatalogContext

---
+++ Services/Catalog.Infrastructure/Data/CatalogContext.cs
@@ -0,0 +1,28 @@
+﻿using Catalog.Core.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.Infrastructure.Data;
+
+public class CatalogContext : ICatalogContext
+{
+    public IMongoCollection<Product> Products { get; }
+    public IMongoCollection<ProductType> ProductTypes { get; }
+    public IMongoCollection<ProductBrand> ProductBrands { get; }
+
+    public CatalogContext(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+        //Get all collections:
+        Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:ProductsCollection"));
+        ProductTypes = database.GetCollection<ProductType>(configuration.GetValue<string>("DatabaseSettings:BrandsCollection"));
+        ProductBrands = database.GetCollection<ProductBrand>(configuration.GetValue<string>("DatabaseSettings:TypesCollection"));
+
+        //Seed data:
+        ProductSeedData.SeedData(Products);
+        ProductTypeSeedData.SeedData(ProductTypes);
+        ProductBrandSeedData.SeedData(ProductBrands);
+    }
+}
---
+++ Services/Catalog.Infrastructure/Data/ICatalogContext.cs
@@ -0,0 +1,11 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Infrastructure.Data;
+
+public interface ICatalogContext
+{
+    IMongoCollection<Product> Products { get; }
+    IMongoCollection<ProductType> ProductTypes { get; }
+    IMongoCollection<ProductBrand> ProductBrands { get; }
+}
