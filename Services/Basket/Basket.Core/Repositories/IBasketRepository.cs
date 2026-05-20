edf00a383c0749d89e5af10f345157d49768f80f
MHGalaxy <mo.ho.kahkeshan@gmail.com>
2026-05-20 21:18:19 +0330

    Create basket repository

--- Services/Basket/Basket.Core/Repositories/IBasketRepository.cs
+++ Services/Basket/Basket.Core/Repositories/IBasketRepository.cs
@@ -4,7 +4,7 @@ namespace Basket.Core.Repositories;
 
 public interface IBasketRepository
 {
-    Task<ShoppingCart> GetBasketAsync(string userName);
-    Task<ShoppingCart> UpdateBasketAsync(ShoppingCart shoppingCart);
+    Task<ShoppingCart?> GetBasketAsync(string userName);
+    Task<ShoppingCart?> UpdateBasketAsync(ShoppingCart shoppingCart);
     Task<bool> DeleteBasketAsync(string userName);
 }
--- Services/Basket/Basket.Infrastructure/Repositories/BasketRepository.cs
+++ Services/Basket/Basket.Infrastructure/Repositories/BasketRepository.cs
@@ -1,22 +1,27 @@
 ﻿using Basket.Core.Entities;
 using Basket.Core.Repositories;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 
 namespace Basket.Infrastructure.Repositories;
 
-public class BasketRepository : IBasketRepository
+public class BasketRepository(IDistributedCache redis) : IBasketRepository
 {
-    public async Task<ShoppingCart> GetBasketAsync(string userName)
+    public async Task<ShoppingCart?> GetBasketAsync(string userName)
     {
-        throw new NotImplementedException();
+        var basket = await redis.GetStringAsync(userName);
+        return string.IsNullOrEmpty(basket) ? null : JsonConvert.DeserializeObject<ShoppingCart>(basket);
     }
 
-    public async Task<ShoppingCart> UpdateBasketAsync(ShoppingCart shoppingCart)
+    public async Task<ShoppingCart?> UpdateBasketAsync(ShoppingCart shoppingCart)
     {
-        throw new NotImplementedException();
+        await redis.SetStringAsync(shoppingCart.UserName, JsonConvert.SerializeObject(shoppingCart));
+        return await GetBasketAsync(shoppingCart.UserName);
     }
 
     public async Task<bool> DeleteBasketAsync(string userName)
     {
-        throw new NotImplementedException();
+        await redis.RemoveAsync(userName);
+        return true;
     }
 }
