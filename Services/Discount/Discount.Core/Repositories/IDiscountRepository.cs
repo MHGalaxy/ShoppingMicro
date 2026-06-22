7bfc64291428e50f6708691d9e80e1e5fcb6c4a8
MHGalaxy <mo.ho.kahkeshan@gmail.com>
2026-06-22 17:24:54 +0330

    Add Coupon entity Add IDiscooutRepository interface

---
+++ Services/Discount/Discount.Core/Repositories/IDiscountRepository.cs
@@ -0,0 +1,11 @@
+﻿using Discount.Core.Entities;
+
+namespace Discount.Core.Repositories;
+
+public interface IDiscountRepository
+{
+    Task<Coupon> GetDiscount(string productId);
+    Task<bool> CreateDiscount(Coupon coupon);
+    Task<bool> UpdateDiscount(Coupon coupon);
+    Task<bool> DeleteDiscount(string productId);
+}
