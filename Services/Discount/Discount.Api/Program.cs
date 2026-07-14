a5a77d488e5db3059886e29823ba4a7e7fc23865
MHGalaxy <mo.ho.kahkeshan@gmail.com>
2026-07-14 18:31:41 +0330

    Add DbExtension in Discount service for seeding data.

--- Services/Discount/Discount.Api/Program.cs
+++ Services/Discount/Discount.Api/Program.cs
@@ -1,3 +1,5 @@
+using Discount.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,8 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+app.MigrateDatabase();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
